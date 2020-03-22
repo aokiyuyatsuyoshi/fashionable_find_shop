00eec7e5c1fdfb65458ae256fed06579323c6d61
aokiyuyatsuyoshi <rainarrow0114@ezweb.ne.jp>
2020-03-23 03:08:26 +0900

    Initial Commit

---
+++ find_shop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  find_shop
+//
+//  Created by Yuya Aoki on 2020/03/23.
+//  Copyright © 2020 Yuya Aoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
